acf5477e9f29855f24adffd9dc48414b4504177f
Kazuki Sawada <kazuki@6715.jp>
2019-07-08 21:41:05 +0900

    upd

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"crypto/tls"
 	"fmt"
 	"io/ioutil"
@@ -54,7 +53,9 @@ func gameStarter() {
 		panic(err)
 	}
 
-	w := bufio.NewWriterSize(c, 1024)
+	w := bufio.NewWriterSize(c, 16*1024)
+	r := bufio.NewReaderSize(c, 16*1024)
+
 	w.WriteString("GET /internships/2019/games?level=3 HTTP/1.1\r\n")
 	w.WriteString("Host: apiv2.twitcasting.tv\r\n")
 	w.WriteString("Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImI1NTc5YWE2N2VlNDI3MDBmODhiNTQ5ZjVjNjA2MTgxYTM1ODFhZTEzODE1MmM1OTZjMDNjNTJlOTQ4ZjUyOTkyMzRiYjFiN2JjZmE3ZWEzIn0.eyJhdWQiOiIxODIyMjQ5MzguMjNhNzJmNDA2NzI4M2I0OWY5NjZmOTMyMzViMTg2NDQzN2VjNWY2YTlmY2M5NjVlOGIzOTM5MGRmNWQ2YWE5NCIsImp0aSI6ImI1NTc5YWE2N2VlNDI3MDBmODhiNTQ5ZjVjNjA2MTgxYTM1ODFhZTEzODE1MmM1OTZjMDNjNTJlOTQ4ZjUyOTkyMzRiYjFiN2JjZmE3ZWEzIiwiaWF0IjoxNTYyMzMyNzcyLCJuYmYiOjE1NjIzMzI3NzIsImV4cCI6MTU3Nzg4NDc3MSwic3ViIjoiYzphbGljZV9nIiwic2NvcGVzIjpbInJlYWQiXX0.GoWeDo-ZswQ1sp_ejnj9rKT2MwPaZwYpqC_w9GS5r1_bJ-aiPPd8rwUPUY3VFphkSVkVpZRwyGq3bxc1Rx1CQMh5_xBavoaMtCr1iik4YZPTIJJJBjXfpgQTOqRGgKA5HEvz84d4XnQGeFNBCw7zbpfOiBmiByHDfulh_SjkI-AwCUPJ4vaXHVjcHHtqlQfZ5jxYwLH2Zv0duwlsDMxR-tWU70TGeFV71yByE53fL4s6Heg607BeFDFhIvNmkMOULiv5xOrnJlmGrxySfflZn4KbStRydypvfgAc2Kbkz1YUQatQazN4hvCrr-otpyPccdKBLF8cf4UVpwSpCSClzQ\r\n")
@@ -67,14 +68,32 @@ func gameStarter() {
 		panic(err)
 	}
 
-	resp, err := ioutil.ReadAll(c)
+	if _, err = r.ReadBytes('{'); err != nil {
+		panic(err)
+	}
+	if _, err = r.Discard(6); err != nil {
+		panic(err)
+	}
+	id, err := r.ReadBytes('"')
 	if err != nil {
 		panic(err)
 	}
 
-	entry := bytes.Index(resp, []byte("{"))
-	idCh <- string(resp[7+entry : 39+entry])
-	queCh <- []string{string(resp[53+entry : 78+entry]), string(resp[81+entry : 81+entry+bytes.Index(resp[81+entry:], []byte("\""))])}
+	idCh <- string(id[:len(id)-1])
+
+	if _, err = r.Discard(13); err != nil {
+		panic(err)
+	}
+	que1, err := r.ReadBytes('=')
+	if err != nil {
+		panic(err)
+	}
+	que2, err := r.ReadBytes('"')
+	if err != nil {
+		panic(err)
+	}
+
+	queCh <- []string{string(que1[:len(que1)-2]), string(que2[1 : len(que2)-1])}
 
 	wg.Done()
 }
@@ -88,12 +107,14 @@ func answerQueryer() {
 	fmt.Println(rdb.Ping().Result())
 
 	que := <-queCh
+
 	ans, err := rdb.HGet(que[0], que[1]).Result()
 	if err != nil {
 		fmt.Println(">>>", que)
 		deleteGame()
 		panic(err)
 	}
+
 	ansCh <- ans
 
 	wg.Done()
@@ -107,7 +128,7 @@ func answerSender() {
 		panic(err)
 	}
 
-	w := bufio.NewWriterSize(c, 1024)
+	w := bufio.NewWriterSize(c, 16*1024)
 
 	fmt.Println("Answer sending standby ...")
 	w.WriteString("POST /internships/2019/games/" + <-idCh + " HTTP/1.1\r\n")
